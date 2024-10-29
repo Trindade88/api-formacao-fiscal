d1cc8196977cb98865a5cdccaacab9b23086620b
juanitusBR <39406203+juanitusBR@users.noreply.github.com>
2024-10-29 20:25:05 -0300

    Update adicionais.pas

--- src/serializable/adicionais.pas
+++ src/serializable/adicionais.pas
@@ -1,24 +1,2 @@
-unit serializable.adicionais;
-
-interface
-
-uses
-  GBJSON.Attributes,  
-
-type
-  Tadi = class
-  private
-    FnAdicao: Integer;
-    FnSeqAdic: Integer;
-    FcFabricante: String; 
-  public
-    property nAdicao: Integer read FnAdicao write FnAdicao;
-    property nSeqAdic: Integer read FnSeqAdic write FnSeqAdic;    
-    property cFabricante: String read FcFabricante write FcFabricante;    
-  end;
-
-implementation
-
-end.
 
         
