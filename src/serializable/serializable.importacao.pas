3e370e7b51fcc68edc9912191abcf079d7cb9d56
juanitusBR <39406203+juanitusBR@users.noreply.github.com>
2024-10-29 20:05:54 -0300

    Create serializable.importacao.pas
    Tag do produto com informacoes de importacao

---
+++ src/serializable/serializable.importacao.pas
@@ -0,0 +1,48 @@
+unit serializable.importacao;
+
+interface
+
+uses
+  System.Generics.Collections,
+  GBJSON.Attributes,
+  serializable.obscont, serializable.adicionais;
+
+type
+  TDI = class
+  private
+    FnDI: Integer;
+    FdDI: TDateTime;
+    FxLocDesemb: String;
+    FUFDesemb: String;
+    FdDesemb: TDateTime;
+    FcExportador: String;
+    Fadi: TObjectList<Tadi>;    
+  public
+    property nDI: Integer read FnDI write FnDI;
+    property dDI: TDateTime read FdDI write FdDI;
+    property xLocDesemb: String read FxLocDesemb write FxLocDesemb;
+    property UFDesemb: String read FUFDesemb write FUFDesemb;
+    property dDesemb: TDateTime read FdDesemb write FdDesemb;
+    property cExportador: String read FcExportador write FcExportador;
+    property adi: TObjectList<Tadi> read Fadi write Fadi;    
+  end;
+
+implementation
+
+{ TDI }
+
+constructor TDI.Create;
+begin
+  Fadi := TObjectList<Tadi>.Create;  
+end;
+
+destructor TDI.Destroy;
+begin
+  Fadi.Free;  
+  inherited;
+end;
+
+end.
+          
+          
+
