453f6baad1c95108579418b4bd62d4f65db6fe71
juanitusBR <39406203+juanitusBR@users.noreply.github.com>
2024-10-29 20:09:33 -0300

    Create adicionais.pas
    Adicionais de importacao

---
+++ src/serializable/adicionais.pas
@@ -0,0 +1,24 @@
+unit serializable.importacao;
+
+interface
+
+uses
+  GBJSON.Attributes,  
+
+type
+  Tadi = class
+  private
+    FnAdicao: Integer;
+    FnSeqAdic: Integer;
+    FcFabricante: String; 
+  public
+    property nAdicao: Integer read FnAdicao write FnAdicao;
+    property nSeqAdic: Integer read FnSeqAdic write FnSeqAdic;    
+    property cFabricante: String read FcFabricante write FcFabricante;    
+  end;
+
+implementation
+
+end.
+
+        
